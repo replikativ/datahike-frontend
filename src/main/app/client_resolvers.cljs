6ce4d4d02a9d6a8826f613c5e4909015553f903c
Chrislain Razafimahefa <razafima@gmail.com>
2020-05-18 12:07:51 +0200

    Starts on a potential solution to the pblm of the http call being async.

--- src/main/app/client_resolvers.cljs
+++ src/main/app/client_resolvers.cljs
@@ -2,12 +2,13 @@
   (:require
     [com.wsscode.pathom.connect :as pc :refer [defresolver defmutation]]
     [taoensso.timbre :as log]
+    [clojure.core.async :refer [go]]
     [ajax.core :refer [GET PUT DELETE POST]]
     ))
 
 
 (defn fetch-schema []
-  (GET "http://localhost:3000/"
+  (GET "http://localhost:3000/schema"
        {:handler (fn [r] (println "GET schema returned: " r) ;;(swap! state update-in [:schema] #(merge r core-schema))
                    )
         :headers {"Content-Type" "application/transit+json"
@@ -23,12 +24,23 @@
                    "Accept" "application/transit+json"}}))
 
 
+
+
 (defresolver datoms-resolver [env input]
   {::pc/output [{:the-datoms [:datoms/id :datoms/elements]}]}
-  {:the-datoms {:datoms/id       :the-datoms
-                :datoms/elements [[1 :attr 2 3 true]]
 
-                }})
+  ;; {:the-datoms {:datoms/id       :the-datoms
+  ;;               :datoms/elements [[1 :attr 2 3 true]]}}
+
+  (let [name    (-> env :ast :params :name)
+        params  {"name"      name}]
+    (go
+      (let [ _ (fetch-schema)
+            ;;{:keys [body]} (<! (http/get "https://myrest.com/search" {:query-params      params}))
+            ]
+        {:the-datoms {:datoms/id       :the-datoms
+                      :datoms/elements [[1 :attr 2 3 true]]}}
+        ))))
 
 
 (def resolvers [datoms-resolver])
